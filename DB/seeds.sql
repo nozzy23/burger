620240859b5540d63f74e53424ef14721e746c80
Oscar Nunez <noscar07@gmail.com>
2020-03-30 15:42:05 -0400

    finished seed

---
+++ DB/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) values ("CheeseBurger", 0);
+
+INSERT INTO burgers (burger_name, devoured) values ("BaconBurger", 1);
+
+INSERT INTO burgers (burger_name, devoured) values ("VeggieBurger", 0);
+
