a4c312f602cd4aedde7a37680ba21353bd23f11e
Oscar Nunez <noscar07@gmail.com>
2020-05-20 21:36:39 -0400

    fixing sql connection

--- config/connection.js
+++ config/connection.js
@@ -6,13 +6,12 @@ if(process.env.JAWSDB_URL){
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-        host:"localhost",
-        port:8889,
-        user:"root",
-        password:"root",
-        database:"burgers_db"
+        host:'localhost',
+        user:'root',
+        password:'root',
+        database:'burgers_db'
     });
-}
+};
 connection.connect(function(err){
     if(err){
         console.log(err);
